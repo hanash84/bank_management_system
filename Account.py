a8253686914fd731c5cec082018fe1b332ba2d2e
hananehsahraiyan <sahraiyanhananeh@gmail.com>
2025-06-03 04:32:57 +0330

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,23 @@
+from abc import ABC,abstractmethod
+class Account(ABC):
+    def __init__(self,accountnumber,owner,balance):
+        self.__accountnumber=accountnumber
+        self.__owner=owner
+        self.__balance=int(balance)
+
+    def set_balance(self, balance):
+        self.__balance = int(balance)
+    def get_balance(self):
+        return  self.__balance
+    def get_owner(self):
+        return self.__owner
+    def get_accountnumber(self):
+        return self.__accountnumber
+    @abstractmethod
+    def deposit(self,amount):
+        pass
+    @abstractmethod
+    def withdraw(self,amount):
+        pass
+    def display_info(self):
+        print(f"AccountNumber:{self.__accountnumber},balance:{self.__balance},owner:{self.__owner}")
